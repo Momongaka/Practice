23c8c4e687dbf2faa2dbdb6602a32855553e3ecb
Daniel <daniel.formas@gmail.com>
2019-06-12 11:43:35 -0500

    New

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,92 +1,116 @@
 ﻿using System;
+using System.Linq;
 
 namespace Practice
 {
     static class Program
     {
         static ChatOp chat = new ChatOp();
-        private static string ChoiceString { get; set; }
-        private static int Choice { get; set; }
 
         static void Main(string[] args)
         {
-            Menu();
+            do
+            {
+                Menu();
+                if (AskForRetry()) Menu();
+                else break;
+            } while (true);
         }
-
-        private static int Base { get; set; }
-        private static int Height { get; set; }
-        private static double Area { get; set; }
-
-        private static string BaseString { get; set; }
-        private static string HeightString { get; set; }
 
         private static void Menu()
         {
-            chat.TextEd("Choose program: Enter Number");
+            chat.TextEd("Choose program: Enter Number", "cyan");
             chat.TextEd("\n1. Triangle Area", "green");
             chat.TextEd("\n2. Odd or Even\n", "green");
             chat.TextEd("\nHit Escape to Quit\n", "red");
-            Console.Write("Enter Input Here:");
-            ChoiceString = Console.ReadLine();
-            Choice = int.Parse(ChoiceString);
+            var choice = AskForInt("Enter Input Here:");
 
-            switch (Choice)
+            switch (choice)
             {
                 case 1:
-                    CalcArea();
+                    GetTriangleDimensionsFromUser();
                     break;
                 case 2:
                     OddEven();
                     break;
                 default:
-                    Menu();
+                    chat.TextEd("Please select a choice from the list.", "red");
                     break;
             }
         }
 
+        private static bool AskForRetry()
+        {
+            chat.TextEd("Would you like to return to the main menu? (Y/N)");
+            
+            var inputString = Console.ReadLine();
+            char i = inputString?.FirstOrDefault() ?? ' ';
+            bool success;
+            switch (i)
+            {
+                case 'Y':
+                case 'y':
+                    success = true; break;
+                default:
+                    success = false; break;
+            }
 
-        private static void CalcArea()
+            return success;
+        }
+
+        private static void GetTriangleDimensionsFromUser()
         {
             chat.TextEd("\nFind the Area of a Triangle", "red", "center");
 
-            chat.TextEd("What is the base of the triangle?", "cyan");
-            BaseString = Console.ReadLine();
-            Base = int.Parse(BaseString);
+            long triBase = AskForInt("What is the base of the triangle?");
+            var triHeight = AskForInt("What is the height of the triangle?");
 
-            chat.TextEd("What is the height of the triangle?", "cyan");
-            HeightString = Console.ReadLine();
-            Height = int.Parse(HeightString);
+            var triArea = GetArea(triBase, triHeight);
+            chat.TextEd("The area is " + triArea, "green");
+        }
 
-            Area = Base * Height / 2.0;
-            chat.TextEd("The area is " + Area, "green");
-
-            Console.ReadKey();
-
-            Menu();
+        private static double GetArea(long triBase, long triHeight)
+        {
+            return triBase * triHeight / 2.0;
         }
 
         private static void OddEven()
         {
             chat.TextEd("\nWelcome to Odd or Even\n");
-            Console.Write("Enter a Number Here:");
-            var numChoice = Console.ReadLine();
-            var numCheck = long.Parse(numChoice);
+            var numCheck = AskForInt("Please enter a number");
+            var answer = IsOddEven(numCheck);
 
-            var oddEven = numCheck % 2;
-
-            if (oddEven != 0)
-            {
-                chat.TextEd("\nThe Number " + numCheck + " is Even\n", "green");
-                Console.ReadKey();
-                Menu();
-            }
-
-            else
+            if (answer != 0)
             {
                 chat.TextEd("\nThe Number " + numCheck + " is Odd\n", "green");
                 Console.ReadKey();
                 Menu();
             }
+            else
+            {
+                chat.TextEd("\nThe Number " + numCheck + " is Even\n", "green");
+                Console.ReadKey();
+                Menu();
+            }
+        }
+
+        private static double IsOddEven(long numCheck)
+        {
+           return numCheck % 2;
+        }
+
+        private static long AskForInt(string query)
+        {
+            chat.TextEd(query, "cyan");
+            var inputString = Console.ReadLine();
+            long output;
+            while (!long.TryParse(inputString, out output))
+            {
+                chat.TextEd("Please enter an integer.", "cyan");
+                inputString = Console.ReadLine();
+            }
+
+            return output;
         }
     }
 }
